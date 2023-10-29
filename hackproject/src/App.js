5f8de2a9b1887908baa0b3d6bef4bc0ba172b08e
Jacob Suter <suter.68@osu.edu>
2023-10-28 22:59:25 -0400

    somethin...

--- hackproject/src/App.js
+++ hackproject/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
+import Footer from './components/Footer';
 import { BrowserRouter as Router, Routes, Route }
     from 'react-router-dom';
 import Home from './pages';
@@ -14,6 +15,7 @@ function App() {
                 <Route path='/index' element={<Home />} />
                 <Route path='/recycling' element={<Recycling />} />
             </Routes>
+            <Footer />
         </Router>
     );
 }
--- hackproject/src/pages/index.js
+++ hackproject/src/pages/index.js
@@ -2,8 +2,15 @@ import React from 'react';
  
 const Home = () => {
     return (
-        <div>
-            <h1>This is the homepage!</h1>
+        <div class="display-grid">
+            <div><img src="./HubCap.jpeg" alt="Stolen car part."></img><p>HubCap</p></div>
+            <div>2</div>
+            <div>3</div>
+            <div>4</div>
+            <div>5</div>
+            <div>6</div>
+            <div>7</div>
+            <div>8</div>
         </div>
     );
 };
