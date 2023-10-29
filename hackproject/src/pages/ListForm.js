1d6693263bb36bdc793faceb44a3959ca2856d96
Jacob Suter <suter.68@osu.edu>
2023-10-28 23:29:19 -0400

    add form data saving

--- hackproject/src/pages/ListForm.js
+++ hackproject/src/pages/ListForm.js
@@ -1,24 +1,48 @@
 import React from 'react';
+import { useReducer, useState } from 'react';
+
+const formReducer = (state, event) => {
+    return {
+      ...state,
+      [event.name]: event.value
+    }
+   }
 
 function ListForm(){
-    const handleSubmit = event=> {
-        event.preventDefault();
-        alert('Thank you for listing this item!')
-    }
+    const [formData, setFormData] = useReducer(formReducer, {});
+    const [submitting, setSubmitting] = useState(false);
 
+    const handleSubmit = event => {
+    event.preventDefault();
+    setSubmitting(true);
+    alert('Thank you for submitting this item!')
+    setTimeout(() => {
+      setSubmitting(false);
+    }, 3000);
+  }
+
+  const handleChange = event => {
+    setFormData({
+      name: event.target.name,
+      value: event.target.value,
+    });
+  }
     return (
         <div className="wrapper">
-            <h1>Item List Form</h1>
-            <form onSubmit={handleSubmit} >
+        <h1>How About Them Apples</h1>
+        {submitting &&
+            <div>Submtting Form...</div>
+        }
+        <form onSubmit={handleSubmit}>
             <fieldset>
-                <label>
-                    <p>Item Title</p>
-                    <input name = "item title" />
-                </label>
-                </fieldset>
-                <button type="submit">Submit</button>
-            </form>
-        </div>
+            <label>
+                <p>Name</p>
+                <input name="name" onChange={handleChange}/>
+            </label>
+            </fieldset>
+            <button type="submit">Submit</button>
+      </form>
+    </div>
     )
 }
 
