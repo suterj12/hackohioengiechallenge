b8efc6395481e721ab1c782597f50503e55c43b5
Jacob Suter <suter.68@osu.edu>
2023-10-28 20:45:46 -0400

    i frankly don't remember, a footer perhaps

---
+++ hackproject/src/components/Footer/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+ 
+const Footer = () => {
+    return (
+        <footer id="footer">
+            <p>Contributors: Lindsey Allen & Jacob Suter</p>
+        </footer>
+    );
+};
+ 
+export default Footer;
