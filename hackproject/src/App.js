b58527dffd3712bcce6440a8c5a5ee143a4955a8
Jacob Suter <suter.68@osu.edu>
2023-10-29 06:46:15 -0400

    creation of divs from form

--- hackproject/src/App.js
+++ hackproject/src/App.js
@@ -4,46 +4,72 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import { BrowserRouter as Router, Routes, Route }
     from 'react-router-dom';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Recycling from './pages/recycling';
 import ListForm from './pages/ListForm';
 import Events from './pages/Events';
+import { Link } from 'react-router-dom';
 import PlasticDrawers from './pages/Listings/PlasticDrawers';
 
-function Home() {
+function Home(props) {
+
+    useEffect(() => {
+        let stuff = document.getElementById("listings");
+        for (let i = 0; i < props.newListings.length; i++) {
+            stuff.appendChild(props.newListings[i]);
+        }
+        // eslint-disable-next-line
+    }, []);
+
     return(
-    <div id="listings" class="display-grid">
-            <div><img src="./HubCap.jpeg" alt="Stolen car part."></img><p>HubCap</p></div>
-            <div>2</div>
-            <div>3</div>
-            <div>4</div>
-            <div>5</div>
-            <div>6</div>
-            <div>7</div>
-            <div>8</div>
+        <div id="listings" class="display-grid">
+                <div><img src="./drawers.webp" alt="Plastic Drawers"></img>
+                <p><Link to="/Listings/PlasticDrawers">Plastic Drawers</Link>
+                <br></br>On Campus, South</p></div>
+                <div><img src="./beanBag.jpeg" alt="Bean Bag Chair"></img>
+                <p><b>Bean Bag Chair</b>
+                <br></br>Off Campus, Central</p></div>
+                <div><img height="200" src="/calculator.jpeg" alt="TI-84 Calculator"></img>
+                <p><b>TI-84 Calculator</b>
+                <br></br>On Campus, North</p></div>
+                <div><img src="./tshirt.jpeg" alt="Library T-Shirt"></img>
+                <p><b>Library T-Shirt</b>
+                <br></br>Off Campus, North East</p></div>
+                <div><img src="./wallArt.jpeg" alt="Obnoxious Wall Art"></img>
+                <p><b>Obnoxious Wall Art</b>
+                <br></br>Off Campus, South West</p></div>
+                <div><img src="./nokia.jpeg" alt="Nokia 8000 Tough"></img>
+                <p><b>Nokia 8000 Tough</b>
+                <br></br>Off Campus, West</p></div>
+                <div><img height="200" src="./mirror.jpeg" alt="Oval-shapped Mirror"></img>
+                <p><b>Oval-shapped Mirror</b>
+                <br></br>On Campus, South</p></div>
+                <div><img src="./HubCap.jpeg" alt="Hub Cap (2007 Honda Civic)"></img>
+                <p><b>Hub Cap (2007 Honda Civic)</b>
+                <br></br>Off Campus, North East</p></div>
         </div>
     );
 }
 
  
 function App() {
-
+    
+    let listings = [];
     const [inputs, setInputs] = useState({});
 
     const handleChange = (event) => {
-      const name = event.target.name;
-      const value = event.target.value;
-      setInputs(values => ({...values, [name]: value}))
+        const name = event.target.name;
+        const value = event.target.value;
+        setInputs(values => ({...values, [name]: value}))
     }
 
     const handleSubmit = (event) => {
         event.preventDefault();
-        let listings = document.getElementById("listings");
-        let div = document.createElement("div");
-        let p = document.createElement("p");
-        p.textContent = inputs.item;
-        p.appendChild(div);
-        listings.appendChild(div);
+        let listing = document.createElement("div");
+        let name = document.createElement("p");
+        name.textContent = inputs.item;
+        listing.appendChild(name);
+        listings.push(listing);
     }
 
     return (
@@ -52,7 +78,7 @@ function App() {
                 <div id="content-wrap">
                 <Header />
                     <Routes>
-                        <Route path='/index' element={<Home inputs={inputs} />} />
+                        <Route path='/' element={<Home inputs={inputs} newListings={listings}/>} />
                         <Route path='/recycling' element={<Recycling />} />
                         <Route path='/ListForm' element={<ListForm inputs={inputs} onChange={handleChange} handleSubmit={handleSubmit}/>} />
                         <Route path='/Events' element={<Events/>} />
--- hackproject/src/components/Header/index.js
+++ hackproject/src/components/Header/index.js
@@ -7,7 +7,7 @@ const Header = () => {
         <div id="header">
             <h1>Hack OHI/O 11 Engie Challenge</h1>
             <div class="navbar">
-                <Link to="/index">Home</Link>
+                <Link to="/">Home</Link>
                 <Link to="/recycling">Recycling</Link>
                 <Link to="/ListForm">List an Item Form</Link>
                 <Link to="/Events">Events</Link>
